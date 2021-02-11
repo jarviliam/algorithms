1c3a829c52aa0ec75ac02011228d92066321a523
jarviliam <jarviliam@gmail.com>
2021-02-11 23:09:32 +0900

    golang - binary search

--- go/search/binary_search_ex1.go
+++ go/search/binary_search_ex1.go
@@ -1,8 +1,8 @@
 /**
+* https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/do-you-really-understand-binary-search/
 *
 *
-*
-*/
+ */
 package main
 
 import (
@@ -10,24 +10,65 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func load(in string) ([]string, error) {
+func sliceAtoi(a []string) ([]int, error) {
+	sa := make([]int, 0, len(a))
+	for _, v := range a {
+		i, error := strconv.Atoi(v)
+		if error != nil {
+			return sa, error
+		}
+		sa = append(sa, i)
+	}
+	return sa, nil
+}
+
+func load(in string) ([]int, []int, error) {
 	file, err := os.Open(in)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer file.Close()
-	var input []string
+	var input []int
 	scanner := bufio.NewScanner(file)
-	numLines := strconv.Atoi(scanner.Text())
-	/*for scanner.Scan() {
-		input = append(input, scanner.Text())
-	}*/
-	fmt.Println(numLines)
-	return input, scanner.Err()
+	scanner.Scan()
+	//fmt.Println(scanner.Text())
+	scanner.Scan()
+	temp := strings.Split(scanner.Text(), " ")
+	converted, _ := sliceAtoi(temp)
+	scanner.Scan()
+	numInputs, _ := strconv.Atoi(scanner.Text())
+	for i := 0; i < numInputs; i++ {
+		scanner.Scan()
+		v, _ := strconv.Atoi(strings.Split(scanner.Text(), " ")[0])
+		input = append(input, v)
+	}
+	return input, converted, scanner.Err()
+}
+func binarySearch(values []int, target int, lowIdx int, highIdx int) int {
+	if highIdx < lowIdx || len(values) == 0 {
+		return -1
+	}
+	middle := (highIdx + lowIdx) / 2
+	if values[middle] > target {
+		return binarySearch(values, target, lowIdx, middle-1)
+	} else if values[middle] < target {
+		return binarySearch(values, target, middle+1, highIdx)
+	} else {
+		return middle
+	}
 }
 
 func main() {
-
+	points, lines, _ := load("ex1.txt")
+	for _, v := range points {
+		out := binarySearch(lines, v, 0, len(lines)-1)
+		if out == -1 {
+			fmt.Println(0)
+		} else {
+			fmt.Println(out)
+		}
+	}
 }
